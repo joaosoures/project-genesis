cb61576ea26bb368b3b179efc6512665582025aa
João Luiz Sousa <joaoresende2603@gmail.com>
2026-09-08 12:35:57 -0300

    feat: Ajustar a sombra para não ficar cortada nas bordas em nenhum dos dois

--- src/components/console/BlurEdges.tsx
+++ src/components/console/BlurEdges.tsx
@@ -2,8 +2,8 @@
 export default function BlurEdges() {
   return (
     <>
-      <div className="pointer-events-none fixed top-0 inset-x-0 h-20 z-30 blur-edge-top" />
-      <div className="pointer-events-none fixed bottom-0 inset-x-0 h-24 z-30 blur-edge-bottom" />
+      <div className="pointer-events-none fixed top-0 -left-12 -right-12 h-20 z-30 blur-edge-top" />
+      <div className="pointer-events-none fixed bottom-0 -left-12 -right-12 h-24 z-30 blur-edge-bottom" />
     </>
   );
 }
